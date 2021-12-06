2d21433663c9a70f350886a7a13114879652087a
SylwiaLeung <longdepati@gmail.com>
2021-12-06 18:57:37 +0100

    Created interface for account repository

---
+++ LearningMaterials/Models/Repositories/Interfaces/IAccountRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace LearningMaterials.Controllers
+{
+    public interface IAccountRepository
+    {
+        Task RegisterUser(RegisterUserDto dto);
+        Task<string> GenerateJwt(LoginDto dto);
+    }
+}
