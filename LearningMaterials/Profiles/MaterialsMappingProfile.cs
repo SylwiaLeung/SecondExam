0b7c20ed4f12bc01c3ab9f89bace36e46ab449bc
SylwiaLeung <longdepati@gmail.com>
2021-12-06 14:48:00 +0100

    Implement map for materialtypes

--- LearningMaterials/Profiles/MaterialsMappingProfile.cs
+++ LearningMaterials/Profiles/MaterialsMappingProfile.cs
@@ -11,6 +11,10 @@ namespace LearningMaterials.Profiles
             CreateMap<Author, AuthorReadDto>();
             CreateMap<AuthorCreateDto, Author>();
             CreateMap<AuthorUpdateDto, Author>().ReverseMap();
+
+            CreateMap<MaterialType, MaterialTypeReadDto>();
+            CreateMap<MaterialTypeCreateDto, MaterialType>();
+            CreateMap<MaterialTypeUpdateDto, MaterialType>().ReverseMap();
         }
     }
 }
