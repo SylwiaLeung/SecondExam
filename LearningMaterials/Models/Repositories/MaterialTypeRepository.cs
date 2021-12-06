51ad5fc08ed7b57f241a8a5e3c976b943f053913
SylwiaLeung <longdepati@gmail.com>
2021-12-06 14:37:34 +0100

    Implement interface for MaterialType in a basic form

---
+++ LearningMaterials/Models/Repositories/MaterialTypeRepository.cs
@@ -0,0 +1,39 @@
+using LearningMaterials.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LearningMaterials.Models
+{
+    public class MaterialTypeRepository : IMaterialTypeRepository
+    {
+        public Task Create(MaterialType obj)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(MaterialType obj)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<MaterialType>> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<MaterialType> GetSingle(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task SaveAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(MaterialType obj)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
