36f55e040e641f87ed2726207d5e873fe00efb91
SylwiaLeung <longdepati@gmail.com>
2021-12-06 19:02:26 +0100

    Implemented RegisterUserDto

--- LearningMaterials/Models/Dtos/Account/RegisterUserDto.cs
+++ LearningMaterials/Models/Dtos/Account/RegisterUserDto.cs
@@ -2,5 +2,10 @@
 {
     public class RegisterUserDto
     {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string ConfirmPassword { get; set; }
+        public int RoleId { get; set; } = 1;
     }
 }
