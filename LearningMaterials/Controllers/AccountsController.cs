49f3c84d9d985b6717afaa921da0154f7c017272
SylwiaLeung <longdepati@gmail.com>
2021-12-06 18:54:48 +0100

    Implemented Accounts controller

---
+++ LearningMaterials/Controllers/AccountsController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace LearningMaterials.Controllers
+{
+    [Route("api/accounts")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly IAccountRepository _accountRepository;
+
+        public AccountsController(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult> RegisterUser([FromBody] RegisterUserDto dto)
+        {
+            await _accountRepository.RegisterUser(dto);
+
+            return Ok();
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login([FromBody] LoginDto dto)
+        {
+            string token = await _accountRepository.GenerateJwt(dto);
+
+            return Ok(token);
+        }
+    }
+}
