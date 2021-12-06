ed3d653724737f0a0e973e0731b0dc529d09a726
SylwiaLeung <longdepati@gmail.com>
2021-12-06 18:47:56 +0100

    Applied migration to the db

--- LearningMaterials/Migrations/20211206174716_Authentication.Designer.cs
+++ LearningMaterials/Migrations/20211206174716_Authentication.Designer.cs
@@ -4,14 +4,16 @@ using LearningMaterials.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LearningMaterials.Migrations
 {
     [DbContext(typeof(MaterialsDbContext))]
-    partial class MaterialsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211206174716_Authentication")]
+    partial class Authentication
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
