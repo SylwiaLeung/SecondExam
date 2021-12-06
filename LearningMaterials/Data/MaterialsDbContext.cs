dc7b1bc6d0b17ae571acb61b2b2cff6c754f0ebd
SylwiaLeung <longdepati@gmail.com>
2021-12-06 11:10:18 +0100

    Initialize Db class

---
+++ LearningMaterials/Data/MaterialsDbContext.cs
@@ -0,0 +1,17 @@
+using LearningMaterials.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LearningMaterials.Data
+{
+    public class MaterialsDbContext : DbContext
+    {
+        public MaterialsDbContext(DbContextOptions<MaterialsDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<MaterialType> MaterialTypes { get; set; }
+        public DbSet<Material> Materials { get; set; }
+    }
+}
