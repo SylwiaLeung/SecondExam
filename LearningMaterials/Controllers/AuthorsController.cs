5f9909a0aae07d73ad71acda29954f43df2309f1
SylwiaLeung <longdepati@gmail.com>
2021-12-06 12:26:20 +0100

    Create AuthorsController class with methods definitions

---
+++ LearningMaterials/Controllers/AuthorsController.cs
@@ -0,0 +1,55 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace LearningMaterials.Controllers
+{
+    [Route("api/authors")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IAuthorRepository _repository;
+
+        public AuthorsController(IMapper mapper, IAuthorRepository repository)
+        {
+            _mapper = mapper;
+            _repository = repository;
+        }
+
+        //GET api/authors/1
+        [HttpGet("{id}")]
+        public ActionResult<AuthorReadDto> GetAuthor([FromRoute] int id)
+        {
+
+        }
+
+        //GET api/authors
+        [HttpGet]
+        public ActionResult<IEnumerable<AuthorReadDto>> GetAuthors()
+        {
+
+        }
+
+        //POST api/authors
+        [HttpPost]
+        public ActionResult<AuthorReadDto> CreateAuthor([FromBody] AuthorCreateDto createDto)
+        {
+
+        }
+
+        //PUT api/authors/1
+        [HttpPut("{id}")]
+        public ActionResult<AuthorReadDto> UpdateAuthor([FromRoute] int id, [FromBody] AuthorUpdateDto updateDto)
+        {
+
+        }
+
+        //DELETE api/authors/1
+        [HttpDelete("{id}")]
+        public ActionResult DeleteAuthor([FromRoute] int id)
+        {
+
+        }
+    }
+}
